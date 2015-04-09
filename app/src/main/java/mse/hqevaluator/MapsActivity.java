9bb6ef6e38a20e4ed6238a89d5d51f352f13423c
André Gasser <andre.gasser@gmx.ch>
2015-04-09 08:31:47 +0200

    Changed app theme to halo dark. Cleaned up code.

--- app/src/main/java/mse/hqevaluator/MapsActivity.java
+++ app/src/main/java/mse/hqevaluator/MapsActivity.java
@@ -46,8 +46,6 @@ public class MapsActivity extends ActionBarActivity
         dbHelper = new DbHelper(this);
         setUpMapIfNeeded();
         buildGoogleApiClient();
-
-
     }
 
     @Override
@@ -68,16 +66,14 @@ public class MapsActivity extends ActionBarActivity
                 .addConnectionCallbacks(this)
                 .build();
         googleApiClient.connect();
-
     }
 
+    /***
+     * Setup the map control if it was not the case yet.
+     */
     private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             if (mMap != null) {
                 setUpMap();
             }
@@ -92,15 +88,13 @@ public class MapsActivity extends ActionBarActivity
      */
     private void setUpMap() {
         Log.i(this.getLocalClassName(), "setUpMap: Setting up map.");
-        //placeNuclearPowerPlants();
-        //placeMotorwayRamps();
         Log.i(this.getLocalClassName(), "setUpMap: Map set up.");
     }
 
     @Override
     public void onConnected(Bundle connectionHint) {
         location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
-        setlocationtocurent();
+        setCurrentLocation();
         addHeatMap();
 
     }
@@ -115,18 +109,16 @@ public class MapsActivity extends ActionBarActivity
         Helpers.showToast("Connection failed", getApplicationContext());
     }
 
-    private void setlocationtocurent() {
+    private void setCurrentLocation() {
         if (location != null) {
-            // Display toast for debugging purposes
-            //Helpers.showToast("Lat: " + location.getLatitude() + "\nLng: " + location.getLongitude(), getApplicationContext());
-
+            Log.d("MapsActivity", "Current Pos: Lat: " + location.getLatitude() + "\nLng: " + location.getLongitude());
             LatLng pos = new LatLng(location.getLatitude(), location.getLongitude());
             float zoom = 7.0f;    // valid values between 2.0 and 21.0
             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(pos, zoom));
         }
         else {
             // Display toast for debugging purposes
-            Helpers.showToast("Location was empty", getApplicationContext());
+            Log.d("MapsActivity", "Current Pos: Location could not be determined");
         }
     }
 
@@ -134,56 +126,94 @@ public class MapsActivity extends ActionBarActivity
 
         prefs = getSharedPreferences(null, MODE_PRIVATE);
 
-        Log.d(" ","AKW INT IST"+prefs.getInt("nuclear_power_plant",0));
+        Log.d("MapsActivity","AKW INT is "+prefs.getInt("nuclear_power_plant",0));
 
-        //*********************************************************************************NuclearPowerPlant
-        NuclearPowerPlantTable nuclearPowerPlantTable = dbHelper.getNuclearPowerPlantTable();       //init db support
+        // Process nuclear power plants
+        NuclearPowerPlantTable nuclearPowerPlantTable = dbHelper.getNuclearPowerPlantTable();
         List<NuclearPowerPlant> nuclearPowerPlants = nuclearPowerPlantTable.getAll();
 
         Iterator<NuclearPowerPlant> nuclearPowerPlantIterator = nuclearPowerPlants.iterator();
 
-       while(nuclearPowerPlantIterator.hasNext()) {     //iterate to all nuclearPowerplants
+        while(nuclearPowerPlantIterator.hasNext()) {
             NuclearPowerPlant plant = nuclearPowerPlantIterator.next();
-            Log.d("add Circle",":"+plant.Longitude+plant.Latitude);
-            if(prefs.getInt("spinner_nearfar1",0)==1){  //get dropdown value menu from settings
-                addCircle(prefs.getInt("nuclear_power_plant",0)*1000,new LatLng(plant.Latitude, plant.Longitude),192,57,43);    //add red heatspot
+            Log.d("MapsActivity", "Adding circle at Long: " + plant.Longitude + ", Lat:" + plant.Latitude);
+
+            //get dropdown value menu from settings
+            if (prefs.getInt("spinner_nearfar1", 0) == 1) {
+                //add red heat spot
+                int radius = prefs.getInt("nuclear_power_plant", 0) * 1000;
+                LatLng pos = new LatLng(plant.Latitude, plant.Longitude);
+                int r = 192;
+                int g = 57;
+                int b = 43;
+                addCircle(radius, pos, r, g, b);
             }
-            else{
-                addCircle(prefs.getInt("nuclear_power_plant",0)*1000,new LatLng(plant.Latitude, plant.Longitude),46,204,113);   //add green heatspot
+            else {
+                //add green heat spot
+                int radius = prefs.getInt("nuclear_power_plant", 0) * 1000;
+                LatLng pos = new LatLng(plant.Latitude, plant.Longitude);
+                int r = 46;
+                int g = 204;
+                int b = 113;
+                addCircle(radius, pos, r, g, b);
             }
-            }
-        //*********************************************************************************MotorwayRamps
-        MotorwayRampTable motorwayRampsTable = dbHelper.getMotorwayRampTable();         //init db support
+        }
+
+        // Process motorway ramps
+        MotorwayRampTable motorwayRampsTable = dbHelper.getMotorwayRampTable();
         List<MotorwayRamp> motorwayRampsPlants = motorwayRampsTable.getAll();
 
         Iterator<MotorwayRamp> motorwayRampsIterator = motorwayRampsPlants.iterator();
 
-        while(motorwayRampsIterator.hasNext()) {        //iterate to all motorwayramps
+        while(motorwayRampsIterator.hasNext()) {
             MotorwayRamp plant = motorwayRampsIterator.next();
-            Log.d("add Circle",":"+plant.Longitude+plant.Latitude);
-            if(prefs.getInt("spinner_nearfar2",0)==1){  //get dropdown value menu from settings
-                addCircle(prefs.getInt("motorway_ramp",0)*1000,new LatLng(plant.Latitude, plant.Longitude),192,57,43);      //add red heatspot
+            Log.d("add Circle", ":" + plant.Longitude + plant.Latitude);
+
+            //get dropdown value menu from settings
+            if(prefs.getInt("spinner_nearfar2" ,0) == 1){
+                // add red heat spot
+                int radius = prefs.getInt("motorway_ramp", 0) * 1000;
+                LatLng pos = new LatLng(plant.Latitude, plant.Longitude);
+                int r = 192;
+                int g = 57;
+                int b = 43;
+                addCircle(radius, pos, r, g, b);
             }
             else{
-                addCircle(prefs.getInt("motorway_ramp",0)*1000,new LatLng(plant.Latitude, plant.Longitude),46,204,113);     //add green heatspot
+                // add green heat spot
+                int radius = prefs.getInt("motorway_ramp", 0) * 1000;
+                LatLng pos = new LatLng(plant.Latitude, plant.Longitude);
+                int r = 46;
+                int g = 204;
+                int b = 113;
+                addCircle(radius, pos, r, g, b);
             }
         }
-
     }
 
-    public void addCircle(int radius,LatLng pos,int r,int g, int b){        //add Heatspot
+    /***
+     * Adds a new heat spot in a circular shape.
+     *
+     * @param radius
+     * @param pos
+     * @param r
+     * @param g
+     * @param b
+     */
+    public void addCircle(int radius, LatLng pos,int r, int g, int b){
         int alpha = 170;
-        for(int i=radius/5;i<=radius;i+=radius/5)   //draw 5 circles per heatspot
-        {
 
+        // draw 5 circles per heat spot
+        for(int i = radius / 5;i <= radius; i += radius / 5)
+        {
             CircleOptions circleOptions = new CircleOptions()
-                    .center(pos)   //set center
-                    .radius(i)   //set radius in meters
-                    .fillColor(Color.argb(alpha,r,g,b))  //default
+                    .center(pos)                           //set center
+                    .radius(i)                             //set radius in meters
+                    .fillColor(Color.argb(alpha, r, g, b)) //default
                     .strokeWidth(0);
 
-            Circle circle= mMap.addCircle(circleOptions);
-            alpha = alpha -35; //reduce the alpha in outer circles
+            Circle circle = mMap.addCircle(circleOptions);
+            alpha = alpha - 35; //reduce the alpha in outer circles
         }
     }
 }
